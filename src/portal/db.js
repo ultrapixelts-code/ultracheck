68cee01ed76a6740d767c496af3a82a7d0bb7dfa
ultrapixelts-code <ultrapixel.ts@gmail.com>
2026-01-28 14:01:00 +0100

    Update db.js

--- src/portal/db.js
+++ src/portal/db.js
@@ -1,8 +1,7 @@
 import pg from "pg";
+const { Pool } = pg;
 
-export const pool = new pg.Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL?.includes("render.com")
-    ? { rejectUnauthorized: false }
-    : undefined
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
